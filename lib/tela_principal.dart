35377085ef0a55b4a09861bf2260e073e4f03788
PauloDev13 <devpgm2020@gmail.com>
2025-09-04 19:23:35 -0300

    Building the Home Screen Interface

--- lib/tela_principal.dart
+++ lib/tela_principal.dart
@@ -14,8 +14,65 @@ class _TelaPrincipalState extends State<TelaPrincipal> {
       appBar: AppBar(
         title: Text('CALCULADORA IMC'),
       ),
-      body: Center(
-        child: Text('Corpo do texto'),
+      body: Column(
+        children: <Widget>[
+          Expanded(child:
+            Row(
+              children: [
+                Expanded(child:
+                  CartaoPadrao(
+                    cor: Color(0xFF9e9e9e),
+                  ),
+                ),
+                Expanded(child:
+                  CartaoPadrao(
+                    cor: Color(0xFF9e9e9e),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(child:
+            CartaoPadrao(cor: Color(0xFF9e9e9e),),
+          ),
+          Expanded(child:
+            Row(
+              children: <Widget>[
+                Expanded(child:
+                  CartaoPadrao(
+                    cor: Color(0xFF9e9e9e),
+                  ),
+                ),
+                Expanded(child:
+                  CartaoPadrao(
+                    cor: Color(0xFF9e9e9e),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      )
+    );
+  }
+}
+
+class CartaoPadrao extends StatelessWidget {
+  // propriedade que será informada
+  final Color cor;
+  // construtor
+  const CartaoPadrao({
+    super.key,
+    required this.cor
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+        color: cor,
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
